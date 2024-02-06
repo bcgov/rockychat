c738e4ba8f1f7f815b2bed0ba1b6a4ce0c58e831
Shelly Xue Han <Shelly.Han@gov.bc.ca>
2024-02-05 16:32:31 -0800

    add ability to reply in threads

--- src/commands/CommandHandler.ts
+++ src/commands/CommandHandler.ts
@@ -1,10 +1,12 @@
 import { driver } from "@rocket.chat/sdk";
-import { IMessage } from "@rocket.chat/sdk/dist/config/messageInterfaces";
 import { CommandList } from "./CommandList";
+import { ExtendedIMessage } from "../interfaces/CommandInt";
+import { ROCKETCHAT_USER, ROCKETCHAT_CHANNEL } from '../constants';
+
 
 export const CommandHandler = async (
   err: unknown,
-  message: IMessage
+  message: ExtendedIMessage
 ): Promise<void> => {
   if (err) {
     console.error(err);
@@ -13,28 +15,39 @@ export const CommandHandler = async (
 
   // it's just IMessage not IMessage[]
   // const message = messages[0];
-  if (!message.msg || !message.rid) {
+  if (!message.msg || !message.rid || !message.u) {
     return;
   }
 
-  // rid: room ID
-  const roomName = await driver.getRoomName(message.rid);
-  // check channel name
-  if (roomName !== "rocky-chat") {
+  /* ignore messages in the following situation:
+  - not in rocky-chat channel
+  - the posting user is Rocky itself
+  - the message is the original post with a reply in the thread
+  */
+  if (message.rid !== ROCKETCHAT_CHANNEL || message.u.username === ROCKETCHAT_USER || message.replies) {
     return;
   }
   const [prefix, commandName] = message.msg.split(" ");
-
+  // when calling rocky:
   if (prefix === "!Rocky") {
+    // set thread for IMessage: threadID is t(tmid) or _id if thread doesn't exist:
+    if(!message.tmid) message.tmid = message._id;
+
+    // check for predefined commands:
     for (const Command of CommandList) {
       if (commandName === Command.name) {
-        await Command.command(message, roomName);
+        await Command.command(message);
         return;
       }
     }
-    await driver.sendToRoom(
-      `I am sorry, but \`${commandName}\` is not a valid command.`,
-      roomName
-    );
+
+    // if not, intake question:
+    const response: ExtendedIMessage = {
+      msg: "pretend to be some smart feedback from AI",
+      rid: message.rid,
+      tmid: message.tmid,
+    };
+    await driver.sendMessage(response);
+
   }
 };
