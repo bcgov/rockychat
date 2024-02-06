683e0dddc91d0585221a25d32d5c6c33a20ab98f
Shelly Xue Han <Shelly.Han@gov.bc.ca>
2024-02-05 16:31:12 -0800

    update env var

--- src/bot.ts
+++ src/bot.ts
@@ -1,18 +1,9 @@
 /*
 Main function of the chatbot
 */
-import { api, driver } from "@rocket.chat/sdk";
-import dotenv from "dotenv";
+import { driver } from "@rocket.chat/sdk";
 import { CommandHandler } from "./commands/CommandHandler";
-dotenv.config();
-
-const {
-  ROCKETCHAT_URL,
-  ROCKETCHAT_USER,
-  ROCKETCHAT_PASSWORD,
-  ROCKETCHAT_USE_SSL,
-  ROCKETCHAT_CHANNEL,
-} = process.env;
+import { ROCKETCHAT_URL, ROCKETCHAT_USER, ROCKETCHAT_PASSWORD, ROCKETCHAT_CHANNEL, ROCKETCHAT_USE_SSL } from './constants';
 
 if (!ROCKETCHAT_URL || !ROCKETCHAT_USER || !ROCKETCHAT_PASSWORD || !ROCKETCHAT_CHANNEL) {
   console.error("Missing required environment variables.");
@@ -24,19 +15,10 @@ if (!ROCKETCHAT_URL || !ROCKETCHAT_USER || !ROCKETCHAT_PASSWORD || !ROCKETCHAT_C
   console.log(ROCKETCHAT_USER);
   await driver.connect({ host: ROCKETCHAT_URL, useSsl: ssl });
   
-  //driver.login connects your code to the bot account
-  //api.login generates an API authorization token
-  //   The SDK driver does not have full functionality, so some features may require an API call. The api method serves as a wrapper for that call.
-  
   await driver.login({
       username: ROCKETCHAT_USER,
       password: ROCKETCHAT_PASSWORD,
     });
-
-  // await api.login({
-  //   username: ROCKETCHAT_USER,
-  //   password: ROCKETCHAT_PASSWORD,
-  // });
 
   await driver.joinRooms([ROCKETCHAT_CHANNEL]);
   await driver.subscribeToMessages();
