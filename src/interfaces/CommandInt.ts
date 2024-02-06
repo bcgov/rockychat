49148d0d51bf13d20ac63db6d968ad634eb0b72f
Shelly Xue Han <Shelly.Han@gov.bc.ca>
2024-02-05 16:29:56 -0800

    extend IMessage

--- src/interfaces/CommandInt.ts
+++ src/interfaces/CommandInt.ts
@@ -1,7 +1,13 @@
 import { IMessage } from "@rocket.chat/sdk/dist/config/messageInterfaces";
 
+// extend the IMessage interface with more fields supported by API endpoint:
+export interface ExtendedIMessage extends IMessage {
+    tmid?: string;
+    replies?: string[];
+  }
+
 export interface CommandInt {
     name: string;
     description: string;
-    command: (message: IMessage, room: string) => Promise<void>
+    command: (message: ExtendedIMessage) => Promise<void>
 }
